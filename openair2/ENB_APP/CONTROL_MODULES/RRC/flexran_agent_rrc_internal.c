747dd7ab16de17f5ef9ff71fc48ce191ad2016cb
Konstantinos Alexandris <konstantinos.alexandris@eurecom.fr>
2019-05-17 15:05:58 +0200

    Fixes: Memory allocation for offsetFreq parameter and new line for some prints

--- openair2/ENB_APP/CONTROL_MODULES/RRC/flexran_agent_rrc_internal.c
+++ openair2/ENB_APP/CONTROL_MODULES/RRC/flexran_agent_rrc_internal.c
@@ -31,7 +31,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
     /* Set serving cell frequency offset */
     if (trigg->meas_info->has_offset_freq_serving) {
       if (flexran_set_rrc_ofp(mod_id, rnti, trigg->meas_info->offset_freq_serving) < 0) {
-        LOG_E(FLEXRAN_AGENT, "Cannot set Serving cell frequency offset");
+        LOG_E(FLEXRAN_AGENT, "Cannot set Serving cell frequency offset\n");
         return -1;
       }
     }
@@ -39,7 +39,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
     /* Set neighbouring cell frequency offset */
     if (trigg->meas_info->has_offset_freq_neighbouring) {
       if (flexran_set_rrc_ofn(mod_id, rnti, trigg->meas_info->offset_freq_neighbouring) < 0) {
-        LOG_E(FLEXRAN_AGENT, "Cannot set Neighbouring cell frequency offset");
+        LOG_E(FLEXRAN_AGENT, "Cannot set Neighbouring cell frequency offset\n");
         return -1;
       }
     }
@@ -47,14 +47,14 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
     if (trigg->meas_info->n_cell_individual_offset > 0) {
       /* Set the serving cell offset */
       if (flexran_set_rrc_ocp(mod_id, rnti, trigg->meas_info->cell_individual_offset[0]) < 0) {
-        LOG_E(FLEXRAN_AGENT, "Cannot set Serving cell offset");
+        LOG_E(FLEXRAN_AGENT, "Cannot set Serving cell offset\n");
         return -1;
       }
 
       /* Set the neighbouring cell offset */
       for (int i=0; i<(trigg->meas_info->n_cell_individual_offset-1); i++) {
         if (flexran_set_rrc_ocn(mod_id, rnti, i, trigg->meas_info->cell_individual_offset[i+1]) < 0) {
-          LOG_E(FLEXRAN_AGENT, "Cannot set Neighbouring cell offset");
+          LOG_E(FLEXRAN_AGENT, "Cannot set Neighbouring cell offset\n");
           return -1;
         }
       }
@@ -62,7 +62,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
 
     if (trigg->meas_info->has_offset_freq_neighbouring) {
       if (flexran_set_rrc_ofn(mod_id, rnti, trigg->meas_info->offset_freq_neighbouring) < 0)  {
-        LOG_E(FLEXRAN_AGENT, "Cannot set Neighbouring cell frequency offset");
+        LOG_E(FLEXRAN_AGENT, "Cannot set Neighbouring cell frequency offset\n");
         return -1;
       }
     }
@@ -71,7 +71,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
     /* Set rsrp filter coefficient */
     if (trigg->meas_info->has_filter_coefficient_rsrp) {
       if (flexran_set_filter_coeff_rsrp(mod_id, rnti, trigg->meas_info->filter_coefficient_rsrp) < 0) {
-        LOG_E(FLEXRAN_AGENT, "Cannot set RSRP filter coefficient");
+        LOG_E(FLEXRAN_AGENT, "Cannot set RSRP filter coefficient\n");
         return -1;
       }
     }
@@ -79,7 +79,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
     /* Set rsrq filter coefficient */
     if (trigg->meas_info->has_filter_coefficient_rsrq) {
       if (flexran_set_filter_coeff_rsrq(mod_id, rnti, trigg->meas_info->filter_coefficient_rsrq) < 0) {
-        LOG_E(FLEXRAN_AGENT, "Cannot set RSRQ filter coefficient");
+        LOG_E(FLEXRAN_AGENT, "Cannot set RSRQ filter coefficient\n");
         return -1;
       }
     }
@@ -92,7 +92,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
       /* Set Periodic event maximum number of reported cells */
         if (trigg->meas_info->event->periodical->has_max_report_cells) {
           if (flexran_set_rrc_per_event_maxReportCells(mod_id, rnti, trigg->meas_info->event->periodical->max_report_cells) < 0) {
-            LOG_E(FLEXRAN_AGENT, "Cannot set Periodic event max");
+            LOG_E(FLEXRAN_AGENT, "Cannot set Periodic event max\n");
             return -1;
           }
         }
@@ -104,7 +104,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
       /* Set A3 event a3 offset */
         if (trigg->meas_info->event->a3->has_a3_offset) {
           if (flexran_set_rrc_a3_event_a3_offset(mod_id, rnti, trigg->meas_info->event->a3->a3_offset) < 0) {
-            LOG_E(FLEXRAN_AGENT, "Cannot set A3 event offset");
+            LOG_E(FLEXRAN_AGENT, "Cannot set A3 event offset\n");
             return -1;
           }
         }
@@ -112,7 +112,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
       /* Set A3 event report on leave */
         if (trigg->meas_info->event->a3->has_report_on_leave) {
           if (flexran_set_rrc_a3_event_reportOnLeave(mod_id, rnti, trigg->meas_info->event->a3->report_on_leave) < 0) {
-            LOG_E(FLEXRAN_AGENT, "Cannot set A3 event report on leave");
+            LOG_E(FLEXRAN_AGENT, "Cannot set A3 event report on leave\n");
             return -1;
           }
         }
@@ -120,7 +120,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
       /* Set A3 event hysteresis */
         if (trigg->meas_info->event->a3->has_hysteresis) {
           if (flexran_set_rrc_a3_event_hysteresis(mod_id, rnti, trigg->meas_info->event->a3->hysteresis) < 0) {
-            LOG_E(FLEXRAN_AGENT, "Cannot set A3 event hysteresis");
+            LOG_E(FLEXRAN_AGENT, "Cannot set A3 event hysteresis\n");
             return -1;
           }
         }
@@ -128,7 +128,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
       /* Set A3 event time to trigger */
         if (trigg->meas_info->event->a3->has_time_to_trigger) {
           if (flexran_set_rrc_a3_event_timeToTrigger(mod_id, rnti, trigg->meas_info->event->a3->time_to_trigger) < 0) {
-            LOG_E(FLEXRAN_AGENT, "Cannot set A3 event time to trigger");
+            LOG_E(FLEXRAN_AGENT, "Cannot set A3 event time to trigger\n");
             return -1;
           }
         }
@@ -136,7 +136,7 @@ int update_rrc_reconfig(mid_t mod_id, rnti_t rnti, Protocol__FlexRrcTriggering *
       /* Set A3 event maximum number of reported cells */
         if (trigg->meas_info->event->a3->has_max_report_cells) {
           if (flexran_set_rrc_a3_event_maxReportCells(mod_id, rnti, trigg->meas_info->event->a3->max_report_cells) < 0) {
-            LOG_E(FLEXRAN_AGENT, "Cannot set A3 event max report cells");
+            LOG_E(FLEXRAN_AGENT, "Cannot set A3 event max report cells\n");
             return -1;
           }
         }
